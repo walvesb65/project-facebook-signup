bfbf729d23abf11db163a4ad442385c965b2fec7
walvesb65 <willian.alvesb65@gmail.com>
2021-04-03 21:26:59 -0300

    Implementa validação de preenchimento dos imputs cadastrais

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ function alertaButton() {
   alert(referencia);
 }
 button.addEventListener('click', alertaButton);
+
+/* Validar se todos os campos foram preenchidos ao clicar no botão Cadastre-se */
+const rightInp = document.querySelectorAll('.right-content input[type="text"]');
+const passInp = document.querySelector('.right-content input[type="password"]');
+const divRadio = document.querySelectorAll('#buttons input[type="radio"]');
+
+function validacaoentradas() {
+  let resultTrue = true;
+  const resultFalse = false;
+  for (let index = 0; index < rightInp.length; index += 1) {
+    if ((rightInp[index].value && passInp.value) === '') {
+      resultTrue = resultFalse;
+    }
+  }
+  return resultTrue;
+}
+
+function validateRadios() {
+  const resultTrue = true;
+  let resultFalse = false;
+  for (let index = 0; index < divRadio.length; index += 1) {
+    if (divRadio[index].checked) {
+      resultFalse = resultFalse || resultTrue;
+    }
+  }
+  return resultFalse;
+}
+
+const buttonRegister = document.getElementById('facebook-register');

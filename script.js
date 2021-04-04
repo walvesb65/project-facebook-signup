c488892aaa2d6471fbd4d531f18ddf897e6df209
walvesb65 <willian.alvesb65@gmail.com>
2021-04-03 22:24:04 -0300

    Adiciona um novo campo de texto no formulário se a pessoa usuária selecionar a opção Personalizado no campo Gênero

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ button.addEventListener('click', alertaButton);
 /* Validar se todos os campos foram preenchidos ao clicar no botão Cadastre-se */
 const rightInp = document.querySelectorAll('.right-content input[type="text"]');
 const passInp = document.querySelector('.right-content input[type="password"]');
-const divRadio = document.querySelectorAll('#buttons input[type="radio"]');
+const divRadio = document.querySelectorAll('.buttons input[type="radio"]');
 
-function validacaoentradas() {
+function validateInputs() {
   let resultTrue = true;
   const resultFalse = false;
   for (let index = 0; index < rightInp.length; index += 1) {
@@ -61,3 +61,43 @@ radioCustomized.addEventListener('change', selectCustomized);
 radioFemale.addEventListener('change', removeCustomized);
 
 radioMale.addEventListener('change', removeCustomized);
+
+/* Adicione um novo campo de texto no formulário se a pessoa usuária selecionar a opção Personalizado no campo Gênero */
+
+function createElements(elements, text) {
+  const createElement = document.createElement(elements);
+  createElement.innerHTML = text;
+  return createElement;
+}
+
+function createDiv() {
+  const rightContent = document.querySelector('.right-content');
+  const firstname = document.querySelector('input[name="firstname"]').value;
+  const lastname = document.querySelector('input[name="lastname"]').value;
+  const phoneEmail = document.querySelector('input[name="phone_email"]').value;
+  const birthday = document.querySelector('input[name="birthdate"]').value;
+  const gender = [...document.querySelectorAll('#radio-gender input')];
+  rightContent.innerHTML = '';
+  rightContent.appendChild(createElements('p',
+    `Olá, ${firstname} ${lastname}`));
+  rightContent.appendChild(createElements('p',
+    `email/ telefone: ${phoneEmail}`));
+  rightContent.appendChild(createElements('p',
+    `Data de nascimento: ${birthday}`));
+  rightContent.appendChild(createElements('p',
+    `Gênero: ${gender.filter((x) => x.checked)[0].value}`));
+}
+
+function validateForm() {
+  buttonRegister.addEventListener('click', (event) => {
+    const functionInputsText = validateInputs();
+    const functionInputsRadio = validateRadios();
+    if ((functionInputsText && functionInputsRadio) === false) {
+      event.preventDefault();
+      document.querySelector('.validateError').style.display = 'flex';
+    } else {
+      createDiv();
+    }
+  });
+}
+validateForm();
